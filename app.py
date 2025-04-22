d55e4e77ac68d05531ee79cba18390e023c59893
Dani Moya <danielmoya2002@gmail.com>
2025-04-22 20:06:15 +0200

    Update deployment configuration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_frozen import Freezer
+import os
 
 app = Flask(__name__)
 freezer = Freezer(app)
@@ -10,7 +11,10 @@ def index():
 
 if __name__ == '__main__':
     # For development
-    app.run(debug=True)
-    
-    # For generating static files
-    freezer.freeze() 
+    if os.environ.get('FLASK_ENV') == 'development':
+        app.run(debug=True)
+    else:
+        # For generating static files
+        app.config['FREEZER_DESTINATION'] = 'build'
+        app.config['FREEZER_RELATIVE_URLS'] = True
+        freezer.freeze() 
